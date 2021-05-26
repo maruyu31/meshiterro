3b928ad93629d4eb6a7ed17cb00f2562b072634f
maruyu31 <you3746336@gmail.com>
2021-05-26 07:09:47 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c471cde7cac3dde06a1183f3a6b49043f6e4f0468c4b773923582e2da34f363c65605c95d34de2bdbdfd2d687a3a4c1e9f27d6d9a03c6ae45ab749e897a37b99'
